22e8ef2cb599268471c839653e8f54ac1d21c36d
hoonki <chaeefe@gmail.com>
2019-05-28 14:20:20 +0900

    Update yut image & add player

--- src/yutgame/game.java
+++ src/yutgame/game.java
@@ -41,7 +41,7 @@ public class game extends JFrame {
 	 */
 	public game() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 882, 720);
+		setBounds(100, 100, 1032, 922);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -49,36 +49,61 @@ public class game extends JFrame {
 		
 		JLabel lblNewLabel = new JLabel("\uC737\uB180\uC774");
 		lblNewLabel.setFont(new Font("µ¸¿ò", Font.BOLD, 21));
-		lblNewLabel.setBounds(26, 12, 133, 64);
+		lblNewLabel.setBounds(406, 27, 133, 64);
 		contentPane.add(lblNewLabel);
 		
 		JLabel label = new JLabel("");
 		label.setBounds(15, 143, 62, 18);
 		contentPane.add(label);
 		
-		JLabel lblNewLabel_1 = new JLabel("");
-		lblNewLabel_1.setIcon(new ImageIcon("C:\\workspace\\Yutnori\\img\\do.PNG"));
-		lblNewLabel_1.setBounds(583, 77, 171, 252);
-		contentPane.add(lblNewLabel_1);
-		
 		JButton button = new JButton("\uB358\uC9C0\uAE30");
 		button.setBackground(UIManager.getColor("Button.focus"));
 		button.setFont(new Font("µ¸¿ò", Font.BOLD, 30));
-		button.setBounds(522, 349, 285, 64);
+		button.setBounds(730, 699, 213, 64);
 		contentPane.add(button);
-		
-		JButton btnNewButton = new JButton("");
-		btnNewButton.setBackground(new Color(0, 102, 255));
-		btnNewButton.setBounds(442, 476, 45, 52);
-		contentPane.add(btnNewButton);
 		
 		JLabel label_1 = new JLabel("");
 		label_1.setIcon(new ImageIcon("C:\\workspace\\Yutnori\\img\\board.PNG"));
 		
-				label_1.setBounds(25, 88, 476, 448);
+				label_1.setBounds(216, 130, 554, 498);
 				contentPane.add(label_1);
+				
+				JLabel lblPlayer = new JLabel("player1");
+				lblPlayer.setBounds(0, 172, 190, 156);
+				contentPane.add(lblPlayer);
+				
+				JLabel lblPlayer_1 = new JLabel("player2");
+				lblPlayer_1.setBounds(0, 399, 190, 156);
+				contentPane.add(lblPlayer_1);
+				
+				JLabel lblPlayer_2 = new JLabel("player3");
+				lblPlayer_2.setBounds(729, 172, 190, 156);
+				contentPane.add(lblPlayer_2);
+				
+				JLabel lblPlayer_3 = new JLabel("player4");
+				lblPlayer_3.setBounds(739, 333, 190, 156);
+				contentPane.add(lblPlayer_3);
+				
+				JLabel lblNewLabel_2 = new JLabel("New label");
+				lblNewLabel_2.setIcon(new ImageIcon("C:\\workspace\\Yutnori\\img\\front.png"));
+				lblNewLabel_2.setBounds(347, 625, 90, 224);
+				contentPane.add(lblNewLabel_2);
+				
+				JLabel label_2 = new JLabel("New label");
+				label_2.setIcon(new ImageIcon("C:\\workspace\\Yutnori\\img\\backdo.png"));
+				label_2.setBounds(470, 625, 90, 224);
+				contentPane.add(label_2);
+				
+				JLabel label_3 = new JLabel("New label");
+				label_3.setIcon(new ImageIcon("C:\\workspace\\Yutnori\\img\\front.png"));
+				label_3.setBounds(588, 625, 90, 224);
+				contentPane.add(label_3);
+				
+				JLabel label_4 = new JLabel("New label");
+				label_4.setIcon(new ImageIcon("C:\\workspace\\Yutnori\\img\\front.png"));
+				label_4.setBounds(226, 625, 90, 224);
+				contentPane.add(label_4);
 	}
-
 }
 
 
