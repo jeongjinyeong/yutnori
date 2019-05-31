eb4537b57b37c0d84e987a6437b632a50bd672f9
hoonki <chaeefe@gmail.com>
2019-05-31 22:10:33 +0900

    Divide code &Change game.java ->MainFrame.java

--- src/view/MainFrame.java
+++ src/view/MainFrame.java
@@ -19,7 +19,7 @@ import javax.swing.UIManager;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 
-public class game extends JFrame {
+public class MainFrame extends JFrame {
 
 	private JPanel contentPane;
 
@@ -31,7 +31,7 @@ public class game extends JFrame {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					game frame = new game();
+					MainFrame frame = new MainFrame();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -43,7 +43,7 @@ public class game extends JFrame {
 	/**
 	 * Create the frame.
 	 */
-	public game() {
+	public MainFrame() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1070, 700);
 		contentPane = new JPanel();
@@ -51,163 +51,156 @@ public class game extends JFrame {
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
+		JPanel panelBoard = createBoard();
+		panelBoard.setBounds(204, 73, 619, 580);
+		contentPane.add(panelBoard);
+
 		JPanel panelPlayerSet = createPlayerSet();
 		panelPlayerSet.setBounds(4, 80, 186, 512);
 		contentPane.add(panelPlayerSet);
-		
+
 		JPanel panelThrowBtnSet = createThrowSet();
 		panelThrowBtnSet.setBounds(837, 255, 200, 187);
 		contentPane.add(panelThrowBtnSet);
-		
-		JButton btnNewButton_5 = new JButton("New button");
-		btnNewButton_5.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		
-		JButton button_28 = new JButton("New button");
-		button_28.setBounds(566, 424, 45, 45);
-		contentPane.add(button_28);
-		
-		JButton button_24 = new JButton("New button");
-		button_24.setBounds(386, 245, 45, 45);
-		contentPane.add(button_24);
-		
-		JButton button_25 = new JButton("New button");
-		button_25.setBounds(326, 175, 45, 45);
-		contentPane.add(button_25);
-		
-		JButton button_22 = new JButton("New button");
-		button_22.setBounds(631, 175, 45, 45);
-		contentPane.add(button_22);
-		
-		JButton button_26 = new JButton("New button");
-		button_26.setBounds(398, 410, 45, 45);
-		contentPane.add(button_26);
-		
-		JButton button_29 = new JButton("New button");
-		button_29.setBounds(631, 485, 45, 45);
-		contentPane.add(button_29);
-		
-		JButton button_27 = new JButton("New button");
-		button_27.setBounds(326, 485, 45, 45);
-		contentPane.add(button_27);
-		
-		JButton button_23 = new JButton("New button");
-		button_23.setBounds(566, 245, 45, 45);
-		contentPane.add(button_23);
-		
-		JButton button_15 = new JButton("New button");
-		button_15.setBounds(239, 290, 45, 45);
-		contentPane.add(button_15);
-		
-		JButton button_17 = new JButton("New button");
-		button_17.setBounds(239, 467, 45, 45);
-		contentPane.add(button_17);
-		
-		JButton button_19 = new JButton("New button");
-		button_19.setBounds(441, 571, 45, 45);
-		contentPane.add(button_19);
-		
-		JButton button_16 = new JButton("New button");
-		button_16.setBounds(239, 377, 45, 45);
-		contentPane.add(button_16);
-		
-		JButton button_20 = new JButton("New button");
-		button_20.setBounds(526, 571, 45, 45);
-		contentPane.add(button_20);
-		
-		JButton button_18 = new JButton("New button");
-		button_18.setBounds(347, 571, 45, 45);
-		contentPane.add(button_18);
-		
-		JButton button_21 = new JButton("New button");
-		button_21.setBounds(615, 571, 45, 45);
-		contentPane.add(button_21);
-		
-		JButton button_14 = new JButton("New button");
-		button_14.setBounds(239, 197, 45, 45);
-		contentPane.add(button_14);
-		
-		JButton button_12 = new JButton("New button");
-		button_12.setBounds(526, 88, 45, 45);
-		contentPane.add(button_12);
-		
-		JButton button_13 = new JButton("New button");
-		button_13.setBounds(441, 88, 45, 45);
-		contentPane.add(button_13);
-		
-		JButton button_11 = new JButton("New button");
-		button_11.setBounds(347, 88, 45, 45);
-		contentPane.add(button_11);
-		
-		JButton button_10 = new JButton("New button");
-		button_10.setBounds(615, 88, 45, 45);
-		contentPane.add(button_10);
-		
-		JButton button_9 = new JButton("New button");
-		button_9.setBounds(218, 556, 80, 74);
-		contentPane.add(button_9);
-		
-		JButton button_8 = new JButton("New button");
-		button_8.setBounds(218, 73, 80, 74);
-		contentPane.add(button_8);
-		
-		JButton button_7 = new JButton("New button");
-		button_7.setBounds(469, 313, 80, 74);
-		contentPane.add(button_7);
-		btnNewButton_5.setBounds(722, 458, 45, 45);
-		contentPane.add(btnNewButton_5);
-		
-		JButton btnNewButton_4 = new JButton("New button");
-		btnNewButton_4.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnNewButton_4.setBounds(704, 556, 80, 74);
-		contentPane.add(btnNewButton_4);
-		
-		JLabel lblNewLabel = new JLabel("\uC737\uB180\uC774");
-		lblNewLabel.setFont(new Font("µ¸¿ò", Font.BOLD, 21));
-		lblNewLabel.setBounds(15, 12, 133, 64);
-		contentPane.add(lblNewLabel);
-		
-		JLabel label = new JLabel("");
-		label.setBounds(15, 143, 62, 18);
-		contentPane.add(label);
-				
-				JLabel lblNewLabel_1 = new JLabel("");
-				lblNewLabel_1.setIcon(new ImageIcon("C:\\workspace\\yutnori\\img\\moe.png"));
-				lblNewLabel_1.setBounds(837, 73, 200, 173);
-				contentPane.add(lblNewLabel_1);
-						
-						JButton button_3 = new JButton("New button");
-						button_3.setBounds(722, 377, 45, 45);
-						contentPane.add(button_3);
-						
-						JButton button_4 = new JButton("New button");
-						button_4.setBounds(722, 285, 45, 45);
-						contentPane.add(button_4);
-								
-								JButton button_5 = new JButton("New button");
-								button_5.setBounds(722, 197, 45, 45);
-								contentPane.add(button_5);
 										
-										JButton button_6 = new JButton("New button");
-										button_6.setBounds(704, 73, 80, 74);
-										contentPane.add(button_6);
+		JLabel lblHeadLine = new JLabel("\uC737\uB180\uC774");
+		lblHeadLine.setFont(new Font("µ¸¿ò", Font.BOLD, 21));
+		lblHeadLine.setBounds(15, 12, 133, 64);
+		contentPane.add(lblHeadLine);
 										
-										JLabel label_1 = new JLabel("");
-										label_1.setIcon(new ImageIcon("C:\\workspace\\yutnori\\img\\board.PNG"));
-										
-												label_1.setBounds(198, 60, 625, 581);
-												contentPane.add(label_1);
-
-												
-
-
-												
+		JLabel lblResultThrowYut = new JLabel("");
+		lblResultThrowYut.setIcon(new ImageIcon("C:\\workspace\\yutnori\\img\\moe.png"));
+		lblResultThrowYut.setBounds(837, 73, 200, 173);
+		contentPane.add(lblResultThrowYut);
 	
+	}
+	
+	public JPanel createBoard() {
+		JPanel panelBoard = new JPanel();
+		panelBoard.setLayout(null);
+		
+		JButton btn0 = new JButton("");
+		btn0.setBounds(514, 489, 80, 74);
+		panelBoard.add(btn0);
+		
+		JButton btn1 = new JButton("");
+		btn1.setBounds(524, 398, 45, 45);
+		panelBoard.add(btn1);
+		
+		JButton btn2 = new JButton("");
+		btn2.setBounds(524, 307, 45, 45);
+		panelBoard.add(btn2);
+				
+		JButton btn3 = new JButton("");
+		btn3.setBounds(524, 220, 45, 45);
+		panelBoard.add(btn3);		
+		
+		JButton btn4 = new JButton("");
+		btn4.setBounds(524, 125, 45, 45);
+		panelBoard.add(btn4);
+
+		JButton btn5 = new JButton("");
+		btn5.setBounds(502, 12, 80, 74);
+		panelBoard.add(btn5);
+		
+		JButton btn6 = new JButton("");
+		btn6.setBounds(415, 27, 45, 45);
+		panelBoard.add(btn6);		
+		
+		JButton btn7 = new JButton("");
+		btn7.setBounds(325, 27, 45, 45);
+		panelBoard.add(btn7);
+		
+		JButton btn8 = new JButton("");
+		btn8.setBounds(241, 27, 45, 45);
+		panelBoard.add(btn8);
+		
+		JButton btn9 = new JButton("");
+		btn9.setBounds(148, 27, 45, 45);
+		panelBoard.add(btn9);
+		
+		JButton btn10 = new JButton("");
+		btn10.setBounds(26, 12, 80, 74);
+		panelBoard.add(btn10);
+
+		JButton btn11 = new JButton("");
+		btn11.setBounds(35, 125, 45, 45);
+		panelBoard.add(btn11);
+
+		JButton btn12 = new JButton("");
+		btn12.setBounds(35, 220, 45, 45);
+		panelBoard.add(btn12);
+		
+		JButton btn13 = new JButton("");
+		btn13.setBounds(35, 307, 45, 45);
+		panelBoard.add(btn13);
+		
+		JButton btn14 = new JButton("");
+		btn14.setBounds(35, 398, 45, 45);
+		panelBoard.add(btn14);
+		
+		JButton btn15 = new JButton("");
+		btn15.setBounds(14, 486, 80, 74);
+		panelBoard.add(btn15);
+		
+		JButton btn16 = new JButton("");
+		btn16.setBounds(153, 501, 45, 45);
+		panelBoard.add(btn16);
+		
+		JButton btn17 = new JButton("");
+		btn17.setBounds(241, 504, 45, 45);
+		panelBoard.add(btn17);
+		
+		JButton btn18 = new JButton("");
+		btn18.setBounds(325, 504, 45, 45);
+		panelBoard.add(btn18);
+		
+		JButton btn19 = new JButton("");
+		btn19.setBounds(415, 504, 45, 45);
+		panelBoard.add(btn19);
+		
+		JButton btn20 = new JButton("");
+		btn20.setBounds(263, 251, 80, 74);
+		panelBoard.add(btn20);
+		
+		JButton btn21 = new JButton("");
+		btn21.setBounds(441, 107, 45, 45);
+		panelBoard.add(btn21);
+		
+		JButton btn22 = new JButton("");
+		btn22.setBounds(371, 182, 45, 45);
+		panelBoard.add(btn22);
+
+		JButton btn23 = new JButton("");
+		btn23.setBounds(126, 107, 45, 45);
+		panelBoard.add(btn23);
+
+		JButton btn24 = new JButton("");
+		btn24.setBounds(199, 182, 45, 45);
+		panelBoard.add(btn24);
+		
+		JButton btn25 = new JButton("");
+		btn25.setBounds(188, 355, 45, 45);
+		panelBoard.add(btn25);
+		
+		JButton btn26 = new JButton("");
+		btn26.setBounds(126, 414, 45, 45);
+		panelBoard.add(btn26);
+
+		JButton btn27 = new JButton("");
+		btn27.setBounds(371, 344, 45, 45);
+		panelBoard.add(btn27);
+
+		JButton btn28 = new JButton("");
+		btn28.setBounds(441, 414, 45, 45);
+		panelBoard.add(btn28);
+		
+		JLabel lblBoard = new JLabel("");
+		lblBoard.setIcon(new ImageIcon("C:\\workspace\\yutnori\\img\\board.PNG"));
+		lblBoard.setBounds(0, 0, 605, 568);
+		panelBoard.add(lblBoard);
+		
+		return panelBoard;
 	}
 	public JPanel createThrowSet() {
 		
