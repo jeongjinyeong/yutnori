19034e495b78efcdbba445653cd9af116fe7e042
hoonki <chaeefe@gmail.com>
2019-06-04 17:38:51 +0900

    Update start page combobox

--- src/view/StartPage.java
+++ src/view/StartPage.java
@@ -16,6 +16,7 @@ import java.awt.Panel;
 import javax.swing.JTextField;
 import java.awt.Color;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.UIManager;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -32,6 +33,8 @@ public class StartPage extends JFrame{
 	private Controller control;
 	
 	public StartPage() {
+		String[] playerStringSet = {"2명","3명","4명"};
+		String[] horseStringSet = {"2개","3개","4개","5개"};
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 500, 360);
@@ -39,62 +42,37 @@ public class StartPage extends JFrame{
 		startPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(startPane);
 		startPane.setLayout(null);
-		createPlayerNum();
-		createMalNum();
-		createStartGame();
-
-		
-		this.setVisible(true);
-	}
-	
-	
-	public void createPlayerNum() {
-
+		JLabel label_2 = new JLabel("윷놀이");
+		label_2.setBounds(230, 24, 74, 18);
+		startPane.add(label_2);
 		JLabel label = new JLabel("시작인원");
-		label.setBounds(50, 83, 62, 18);
+		label.setBounds(150, 83, 62, 18);
 		startPane.add(label);
-		String[] player_num = {"2명","3명","4명"};
-		playerNum = new JButton[3];
-		for(int i=0; i<3; i++) {
-			playerNum[i] = new JButton(player_num[i]);
-			playerNum[i].setBounds(165+(i*85), 67, 55, 50);
-			startPane.add(playerNum[i]);
-		}		
-
-		
-	}
-
-	public void createMalNum() {
-		
 		JLabel label_1 = new JLabel("말갯수");
-		label_1.setBounds(50, 168, 62, 18);
+		label_1.setBounds(150, 168, 62, 18);
 		startPane.add(label_1);
 
-		String[] mal_num = {"2개","3개","4개","5개"};
-		malNum = new JButton[4];
-		
-		for(int i=0; i<4; i++) {
-			malNum[i] = new JButton(mal_num[i]);
-			malNum[i].setBounds(126+(i*80), 139, 55, 50);
-			startPane.add(malNum[i]);
-			malNum[i].addActionListener(control);
-		}
 
+		JComboBox playerCombo= new JComboBox(playerStringSet);
+		playerCombo.setBounds(250, 83, 100, 30);
+		startPane.add(playerCombo);
+		JComboBox horseCombo= new JComboBox(horseStringSet);
+		horseCombo.setBounds(250, 159, 100, 30);
+		startPane.add(horseCombo);
 		
-	}
-	
-	public void createStartGame() {
-		
-		
+		/*
+		playerCombo.getSelectedItem().toString();
+		horseCombo.getSelectedItem().toString(); 값 가져오는 메쏘드
+		 */
 		JButton btnNewButton = new JButton("게임시작");
 		btnNewButton.setBounds(152, 217, 204, 39);
 		startPane.add(btnNewButton);
 
-		JLabel label_2 = new JLabel("윷놀이");
-		label_2.setBounds(230, 24, 74, 18);
-		startPane.add(label_2);
-		
-	}	
+
+		this.setVisible(true);
+	}
+	
+	
 	public static void main(String[] args) {
 			new StartPage();
 		}
