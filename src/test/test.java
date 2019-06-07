047afd1ee475adebdabc3c882cb3ad768adffc58
hoonki <chaeefe@gmail.com>
2019-06-07 18:56:19 +0900

    update junit

---
+++ src/test/test.java
@@ -0,0 +1,20 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import model.Yut;
+class test {
+
+	@Test
+	void test() {
+		assertEquals(1,Yut.DOE);
+		assertEquals(2,Yut.GAE);
+		assertEquals(3,Yut.GUL);
+		assertEquals(4,Yut.YOOT);
+		assertEquals(5,Yut.MOE);	
+		assertEquals(0,Yut.BACK_DOE);	
+//		fail("Not yet implemented");
+	}
+
+}
